1e407c46c0ef4e3f5ff17b0b7a60d4e708b0b064
mileto1975 <mileto.di.marco@howest.be>
2019-09-18 15:00:18 +0200

    Add project files.

---
+++ ExampleCustomLayouts/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ExampleCustomLayouts.Models;
+
+namespace ExampleCustomLayouts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShowGames()
+        {
+            return View();
+        }
+
+        public IActionResult AdminHome()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
